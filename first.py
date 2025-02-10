707fa8ce86af3f7f1d81347107d7f2d404a7b943
reneSterling <rene.boat23@gmail.com>
2025-02-10 22:10:19 +0000

    My first github push

---
+++ first.py
@@ -0,0 +1,15 @@
+num = 0
+
+num = int(input ("Please enter a number:\n"))
+
+if num > 0:
+    print ("Your number is positive")
+
+elif num < 0:
+    print ("Your number is negative")
+
+elif num == 0:
+    print ("Your number is zero")
+
+else:
+    print ("What you entered is not a number")

d6420bf01b747cb2711c5704d5de435d1b8826e4
reneSterling <rene.boat23@gmail.com>
2025-02-11 21:15:56 +0000

    Hello Kwasi, this is my second python code

---
+++ second.py
@@ -0,0 +1,18 @@
+#This accepts a name and age from the user and displays name is "Eligible" 
+# if age is greater than 18
+
+u_name = " "
+u_age = 0
+
+u_name = str(input ("Please enter your name:\n"))
+u_age = int(input("Please enter your age:\n"))
+
+if u_age >= 18:
+    print (u_name + ": Eligible to vote")
+else:
+    print ("Sorry you are not eligible to vote")
+
+    
+
+
+

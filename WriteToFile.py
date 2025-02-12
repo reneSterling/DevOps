783155d17054bf32952c1ae68b665462e7b54fd6
Kwasi Addai <mail2kwesi@gmail.com>
2025-02-12 21:18:28 +0000

    Write To File almost complete

--- WriteToFile.py
+++ WriteToFile.py
@@ -1,16 +1,31 @@
 #Accept the string value from the user
 # A calculator accepting 2 numbers and an arithmetic operation and writing to file
 
-data1 = int(input("Please enter your first number:\n"));
-data2 = int(input("Please enter your first number:\n"));
-operator = int(input("Please enter an arithmetic operator:\n"));
-ans = data1 + data2
+num1 = int(input("Please enter your first number:\n"));
+num2 = int(input("Please enter your second number:\n"));
+operator = input("Please enter an arithmetic operator:\n");
+ans = 0
 
-print(ans)
+#Match Case in Python
+
+match operator:
+    case '*':
+        ans = num1 * num2
+    case '/':
+         ans = num1 / num2
+    case '%':
+         ans = num1 % num2
+    case '+':
+        ans = num1 + num2 
+    case '-':
+        ans = num1 + num2
+
+#Multiple concatenation
+print(str(num1) +" "+operator+" "+str(num2)+" = "+str(ans))
 
 #Write to a file
-with open("My First File.txt", "w") as file:
-    file.write(ans)
-    file.close()
+#with open("My First File.txt", "w") as file:
+    #file.write(ans)
+   # file.close()
 
-print("\nThe calculation of youe two numbers is " + ans)
+
